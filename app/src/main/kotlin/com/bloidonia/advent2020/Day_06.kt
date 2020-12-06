50e0d7991696a98a40c16d3af71d69081cc4cb97
Tim Yates <tim@gradle.com>
2020-12-06 11:57:45 +0000

    Comment incase anyone is watching

--- app/src/main/kotlin/com/bloidonia/advent2020/Day_06.kt
+++ app/src/main/kotlin/com/bloidonia/advent2020/Day_06.kt
@@ -9,18 +9,27 @@ class Day_06 {
 fun main() {
     val groupData = textFromResource("/6.txt")
         .split("\n\n")
+
     val part1 = groupData
         .map {
+            // For each group, join all the lines together into one long string
+            // Get a unique set of chars, and get the size of this set
             it.split("\n").joinToString("").toSet().size
         }
-        .sum()
+        .sum() // Then add all these sizes together
+
     println(part1)
 
     val part2 = groupData
         .map {
-            it.split("\n").map { it.toSet() }.reduceRight { s, g -> s.intersect(g) }.size
+            // For each persone in the group, get a set of their answers
+            it.split("\n").map { it.toSet() }
+                // Then find the intersection of all the answers in a groups
+                .reduceRight { s, g -> s.intersect(g) }
+                // And get the size
+                .size
         }
-        .sum()
+        .sum() // Then add these sizes together
 
     println(part2)
 }
