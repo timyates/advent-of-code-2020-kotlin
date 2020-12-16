25ef5258ea83b053b2092f8f2fefd597fdf9013f
Tim Yates <tim@gradle.com>
2020-12-16 23:10:09 +0000

    Functions and immutability are the first casualties of crazy optimization

--- app/src/main/kotlin/com/bloidonia/advent2020/Day_15.kt
+++ app/src/main/kotlin/com/bloidonia/advent2020/Day_15.kt
@@ -1,25 +1,24 @@
 package com.bloidonia.advent2020
 
-class Day_15(val starting: List<Long>) {
+class Day_15(val starting: List<Int>) {
 
-    data class Move(val turn: Int, val number: Long)
+    data class Move(val last: Int, val index: Map<Int, Int>)
 
-    val previous = starting.mapIndexed { idx, n -> Move(idx + 1, n) }.toMutableList()
+    var last = starting.last()
+    val index = starting.dropLast(1).mapIndexed { idx, n -> n to (idx + 1) }.toMap().toMutableMap()
 
-    fun playUntil(turn: Int): Long {
-        for (move in starting.size until turn) {
-            val (prevTurn, prevNumber) = previous.last()
-            val prev = previous.reversed().drop(1).find { it.number == prevNumber }?.turn
-            if (prev == null) {
-                previous.add(Move(prevTurn + 1, 0L))
-            } else {
-                previous.add(Move(prevTurn + 1, (prevTurn - prev).toLong()))
-            }
+    fun playUntil(turn: Int): Int {
+        for (curr in starting.size until turn) {
+            val prevLast = last
+            val lastIdx = index[last]
+            last = if (lastIdx == null) 0 else { curr - lastIdx }
+            index[prevLast] = curr
         }
-        return previous.last().number
+        return last
     }
 }
 
 fun main() {
     println(Day_15(listOf(9, 3, 1, 0, 8, 4)).playUntil(2020))
+    println(Day_15(listOf(9, 3, 1, 0, 8, 4)).playUntil(30000000))
 }
